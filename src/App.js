e1a878c03fc6175008b2bbef0cdbe6c783599260
mohammed-almassri <mohammed.m.almassri@gmail.com>
2025-01-28 19:03:45 -0600

    fix

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <h1>Hello World</h1>
       <p>
-        This will be deployed to CodePipeline. 😅
+        This will be deployed to CodePipeline.
       </p>
     </div>
   );
